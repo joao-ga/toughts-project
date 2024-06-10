61ca488e700f7713ada98bf819d14727f1c89fe7
joaogabiazon <jbiazonferreira@gmail.com>
2024-06-10 12:20:00 -0300

    project dependeces

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+const flash = require('express-flash')
+const conn = require('./db/conn')
+
+const app = express()
+
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
+// sesssion midleware
+app.use(
+    session({
+        name: 'session',
+        secret: "nosso_secret",
+        resave: false,
+        saveUninitialized: false,
+        store: new FileStore({
+            logFn: function() {},
+            path: require('path').join(require('os').tmpdir(), 'sessions'),
+        }),
+        cookie: {
+            secure: false,
+            maxAge: 360000,
+            expires: new Date(Date.now() + 360000),
+            httpOnly: true
+        }
+    }),
+)
+
+// flash messages
+app.use(flash())
+
+// public path
+app.use(express.static('public'))
+
+//set session to ress
+app.use((req, res, next) => {
+    if(req.session.userid) {
+        res.locals.session = req.session
+    }
+
+    next()
+})
+
+conn
+    .sync()
+    .then(()=>{
+        app.listen(3000)
+    })
+    .catch((e) => console.log(e))
