1ca31a47894efd822739a16aaad85612f79dca9c
joaogabiazon <jbiazonferreira@gmail.com>
2024-06-12 11:55:29 -0300

    Creating toughts

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -65,33 +65,37 @@ module.exports = class AuthController {
     static async loginPost(req, res) {
         const {email,password} = req.body
 
-        // find user
-        const user = await User.findOne({where: {email: email}})
+        try {
+            // find user
+            const user = await User.findOne({where: {email: email}})
 
-        if(!user) {
-            req.flash('message', 'Usuário não encontrado')
-            res.render('auth/login')
+            if(!user) {
+                req.flash('message', 'Usuário não encontrado')
+                res.render('auth/login')
 
-            return
+                return
+            }
+
+            //check if passwords match  
+            const passwordMatch = bcrypt.compareSync(password, user.password)
+
+            if(!passwordMatch) {
+                req.flash('message', 'Senha incorreta, tente novamente')
+                res.render('auth/login')
+
+                return
+            }
+
+            // keep user login
+            req.session.userid = user.id
+
+            req.flash('message', 'Autenticação realizada com sucesso!')
+
+            req.session.save(()=>{
+                res.redirect('/')
+            })
+        } catch (e) {
+            console.log(e)
         }
-
-        //check if passwords match  
-        const passwordMatch = bcrypt.compareSync(password, user.password)
-
-        if(!passwordMatch) {
-            req.flash('message', 'Senha incorreta, tente novamente')
-            res.render('auth/login')
-
-            return
-        }
-
-        // keep user login
-        req.session.userid = user.id
-
-        req.flash('message', 'Autenticação realizada com sucesso!')
-
-        req.session.save(()=>{
-            res.redirect('/')
-        })
     }
 }
