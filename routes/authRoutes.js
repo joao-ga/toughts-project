8953b09cd95a98eebe561a279868f4cd98a8f520
joaogabiazon <jbiazonferreira@gmail.com>
2024-06-12 10:50:20 -0300

    login function

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,6 +6,7 @@ router.get('/login', AuthController.login)
 router.get('/register', AuthController.register)
 router.post('/register', AuthController.registerPost)
 router.get('/logout', AuthController.logout)
+router.post('/login', AuthController.loginPost)
 
 
 module.exports = router
