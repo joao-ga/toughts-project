a58a9e791bc1a1232e9b8beb9217831cbccaa306
joaogabiazon <jbiazonferreira@gmail.com>
2024-06-12 11:13:52 -0300

    Create a midleware which verify user are login

--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -2,6 +2,10 @@ const express = require("express")
 const router = express.Router()
 const ToughtsController = require('../controllers/ToughtController')
 
+//midleware
+const checkAuth = require('../helpers/auth').checkAuth
+
+router.get('/dashboard', checkAuth,  ToughtsController.dashboard)
 router.get('/', ToughtsController.showToughts)
 
 
